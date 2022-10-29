8c24412c6fc00fe54f0b252108dd7e4a07056c00
Edwar Garcia <garciapatinoedwar26@gmail.com>
2022-10-29 01:11:58 -0500

    Proyecto en que se implementa  API SendGrid para envio de correos electronicos

---
+++ README.md
@@ -0,0 +1,20 @@
+Este servicio esta escrito en Python.
+
+Este servicio consume una API de SendGrid pra enviar mails alos usuarios, con el fin de notificar la creacion de un usuario.
+
+PASO PARA PODER EJECUTAR EL SERVICIO
+
+REQUISITOS PREVIOS:
+1.Tener Python instalado
+2.python -m pip install --upgrade pip
+3.pip install virtualenv
+
+PASO 1: CREAR EL ENTORNO VIRTUAL
+    python -m venv env
+
+PASO 2: ACTIVAR EL ENTORNO
+    .\env\Scripts\activate
+
+PASO 3: INSTALAR REQUERIMIENTOS
+    pip install -r requirements.txt
+
---
+++ prueba_email_sendgrid.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask
+from flask import request
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from dotenv import load_dotenv
+
+app= Flask(__name__)
+
+load_dotenv()
+
+app.run(debug=True)
+
+@app.route('/e-mail')
+def mail():
+
+    destino=request.args.get("correo_destino")
+    asunto = request.args.get("asunto")
+    mensaje = request.args.get("contenido")
+
+    message = Mail(
+        from_email = 'fadedevs22@gmail.com',
+        to_emails=destino,
+        subject=asunto,
+        html_content=mensaje)
+    try:
+        sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY3'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+        return "Correo enviado exitosamente!"
+    except Exception as e:
+        print(e.message)
+        return "error, el correo no fue enviado"
+
+if __name__ == '__main__':
+    app.run(debug=True)
